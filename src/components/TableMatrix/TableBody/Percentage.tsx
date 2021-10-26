0ce81d73eda44cd54f7cfcf24815703b509ed7f0
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-10-26 10:47:32 +0300

    add Percentage logic

---
+++ src/components/TableMatrix/TableBody/Percentage.tsx
@@ -0,0 +1,27 @@
+import {sumRowNumbers} from 'function/index';
+import {FC, useState, useEffect} from 'react';
+import {ICell} from 'function/index';
+import styles from '../TableBody/TableBody.module.scss';
+
+type IPercentageProps = {
+  row: Array<ICell>;
+  item: ICell;
+};
+
+const Percentage: FC<IPercentageProps> = ({item, row}: IPercentageProps) => {
+  const [sum, setSum] = useState(sumRowNumbers(row));
+
+  useEffect(() => {
+    setSum(sumRowNumbers(row));
+  }, [row]);
+
+  const percent = ((item.Amount * 100) / sum).toFixed();
+
+  return (
+    <div className={styles.percent}>
+      <div className={styles.percentText}>{percent}%</div>
+      <div className={styles.columnPercent} style={{height: percent + '%'}}></div>
+    </div>
+  );
+};
+export default Percentage;
